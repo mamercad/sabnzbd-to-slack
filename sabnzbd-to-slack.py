501cb537e214b27a2b5a6b157eaac42bce5baa91
Mark Mercado <mamercad@gmail.com>
2017-11-21 19:40:57 -0500

    initial commit

---
+++ sabnzbd-to-slack.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python
+
+import datetime
+import json
+import os
+import pprint
+import requests
+import sys
+
+try:
+  (scriptname, notification_type, notification_title,
+   notification_text, parameters) = sys.argv
+  slack_message = {
+    'title': notification_title,
+    'pretext': notification_type,
+    'text': notification_text
+  }
+  response = requests.post(
+    parameters,
+    data=json.dumps(slack_message),
+    headers={'Content-Type': 'application/json'}
+  )
+  if response.status_code != 200:
+    raise ValueError('Slack failed with {0} ({1})'.format(response.text, response.status_code))
+except Exception as e:
+  print e
+  sys.exit(1)
